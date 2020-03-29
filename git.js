f263e5e4098a7b627fb9702a2dc081134ae2173e
Orji Cecilia <ceciliaorji.co@gmail.com>
2020-03-29 09:15:49 +0100

    add existing file

---
+++ git.js
@@ -0,0 +1,16 @@
+let name = "Orji Cecilia Olanma";
+console.log("My name is " + name);
+
+const emailAddress = "ceciliaorji.co@gmail.com";
+console.log("My email-address is " + emailAddress);
+
+let slackUsername = "vivcis"
+console.log("My slack username is " + slackUsername);
+
+const track = "Front-end";
+console.log("The track I am offering is " + track);
+
+var courses = ["Front-end", "Coding"];
+console.log("My couses are " + courses);
+
+
